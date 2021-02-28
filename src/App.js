75a0254fb11dc36b62dbe0c0541d8ea3e01581b2
Liz <lizlarkin@highlandtechnology.com>
2021-02-28 14:40:20 -0800

    changed nav design to include buttons and added associated pages/links using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Nav from './Components/Nav/Nav';
+import About from './Components/Pages/About';
+import Links from './Components/Pages/Links';
+import Portfolio from './Components/Pages/Portfolio';
+import Resume from './Components/Pages/Resume';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path="/About" component={About} />
+          <Route path="/Links" component={Links} />
+          <Route path="/Portfolio" component={Portfolio} />
+          <Route path="/Resume" component={Resume} />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Nav = () => {
 
     const navStyles = {
         fontWeight: "bold",
+
+        linkStyles: {
+            marginLeft: "100px",
+        },
     }
 
     return (
         <div>
-            <nav style = {navStyles} className="navbar navbar-dark bg-dark">
-                <span className="navbar-brand mb-0 h1">Liz Larkin Portfolio</span>
+            <nav style = {navStyles} className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Liz Larkin Portfolio</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li style={navStyles.linkStyles} className="nav-item">
+                                <Link to="/About">About</Link>
+                            </li>
+                            <li style={navStyles.linkStyles} className="nav-item">
+                                <Link to="/Resume">Resume</Link>
+                            </li>
+                            <li style={navStyles.linkStyles} className="nav-item">
+                                <Link to="/Portfolio">Portfolio</Link>
+                            </li>
+                            <li style={navStyles.linkStyles} className="nav-item">
+                                <Link to="/Links">Links</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     )
