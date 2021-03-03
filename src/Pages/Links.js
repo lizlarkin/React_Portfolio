1d0202945d700f42ab785a9e40461a4bfee358b3
Liz <lizlarkin@highlandtechnology.com>
2021-03-02 16:14:17 -0800

    added links content and associated syling

--- src/Pages/Links.js
+++ src/Pages/Links.js
@@ -2,10 +2,36 @@ import React from 'react'
 import Home from './Home'
 
 const Links = () => {
+
+    const linkStyles = {
+        border: "1px solid grey",
+        boxShadow: "0px 5px 10px 0px",
+        backgroundColor: "white",
+        borderRadius: "7px",
+        textAlign: "center",
+        padding: "20px",
+        marginBottom: "100px",
+    }
+
     return (
         <div>
             <h1>Links</h1>
             <Home />
+            <div className="row">
+                <div className="col-sm-2"></div>
+                <div className="col-sm-8" id = "links-div" style={linkStyles}>
+                <div className = "link">
+                    <a href = "https://github.com/lizlarkin" target="_blank">GitHub</a>
+                </div>
+                <div className = "link">
+                    <a href = "https://www.linkedin.com/in/larkinelizabeth" target="_blank">LinkedIn</a>
+                </div>
+                <div className = "link">
+                    <a href = "https://www.highlandtechnology.com/" target="_blank">Highland Technology</a>
+                </div>
+                </div>
+                <div className="col-sm-2"></div>
+            </div>
         </div>
     )
 }
